158ab4aedbdfafb2f6a038a032bba29b4b0a215c
marlo <marlonferrari@ucl.br>
2021-10-29 21:52:11 -0300

    aula do exercicio atualizada

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def estoque():  # put application's code here
     codItem = request.form.get('codItem')
     desc = request.form.get('desc')
     medida = request.form.get('medida')
+    categoria = request.form.get('categoria')
     fabricante = request.form.get('fabricante')
     quantidade = request.form.get('quantidade')
     precunit = request.form.get('precunit')
@@ -22,6 +23,7 @@ def estoque():  # put application's code here
         'codItem':codItem,
         'desc':desc,
         'medida':medida,
+        'categoria':categoria,
         'fabricante':fabricante,
         'quantidade':quantidade,
         'precunit':precunit,
@@ -34,6 +36,24 @@ def estoque():  # put application's code here
     arq = open('estoque.json', mode='w')
     arq.write(json.dumps(dados))
     return render_template('cadastro.html')
+
+@app.route('/relatorio', methods=['get'])
+def relatorio():
+    arq = open('estoque.json', mode='r')
+    dados = json.load(arq)
+    #CRIA UMA LISTA DE CATEGORIAS PRESENTES
+    cat_produtos = []
+    for d in dados:
+        if d['categoria'] not in cat_produtos:
+            cat_produtos.append(d['categoria'])
+    #CRIA DICI PARA CONTAGEM DOS ITENS
+    diciCat = {}
+    for cat in cat_produtos:
+        diciCat[cat] = 0
+    #CONTAGEM DOS ITENS
+    for d in dados:
+        diciCat[d['categoria']] += 1
+    return render_template('relatorio.html', diciCat=diciCat)
 
 if __name__ == '__main__':
     app.run()
